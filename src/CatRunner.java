ae5c05dcf4202ed5f8efccaa3990160ec6a1e262
BT_3W2_05 <vincentl105@nycstudents.net>
2023-02-01 08:37:44 -0500

    Done

--- src/CatRunner.java
+++ src/CatRunner.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
+import java.util.Locale;
 
-public class CatRunner {
+public class    CatRunner {
     public static void main (String [] args) {
         ArrayList<Cat> cats = new ArrayList<Cat>();
 
@@ -15,9 +16,21 @@ public class CatRunner {
         System.out.println (cats);
 
         Cat cat4 = new Cat("amigo");
-        Cat replcaedCat = cats.set(2,cat4);
+        Cat replacedCat = cats.set(2,cat4);
 
         System.out.println(cats);
-        System.out.println(cats.get(2));
+        System.out.println(replacedCat);
+
+        cat4.setName(cat4.getName() + "Meow");
+        System.out.println(cat4);
+
+        for (int i = 0; i < cats.size(); i ++){
+            String newName = cats.get(i).getName().toUpperCase();
+            cats.get(i).setName(newName);
+        }
+        System.out.println(cats);
+
+        cats.add(new Cat("SAM"));
+        System.out.println(cats);
     }
 }
